2c5b3053c2eb3ba4a91ff5fc52fae5a28f2223a5
Luccan97 <luccanielsen97@gmail.com>
2022-03-07 18:29:06 -0300

    G
    G

---
+++ 00_prepareData.R
@@ -0,0 +1,150 @@
+
+install.packages("remotes")
+remotes::install_github("rfsaldanha/microdatasus")
+
+library(microdatasus)
+library(tidyverse)
+library(stringi)
+library(sf)
+# Creating Mortality standarized rate, UF, sex age.
+# data1
+# data2
+# data3
+
+
+# World Standard Population by Sex and Age data
+standard_pop <- vroom::vroom("https://www.opendata.nhs.scot/dataset/4dd86111-7326-48c4-8763-8cc4aa190c3e/resource/2f493d21-fd39-48f9-ad6a-9b2c95b32e30/download/world_standard_population_by_sex.csv")
+
+## Cleaning agegroup variable, changings patterns of categorical variables
+standard_pop_clean <- standard_pop %>%
+  mutate(
+    AgeGroup = str_replace_all(AgeGroup, "years", ""),
+    AgeGroup = str_replace_all(AgeGroup, "plus", ""),  
+    AgeGroup = str_replace_all(AgeGroup, " ", "")) %>% 
+  rename(pop = WorldStandardPopulation,
+         agegroup = AgeGroup) %>%
+  mutate(Sex = case_when(Sex == "Male" ~ "m",
+                         Sex == "Female" ~ "f"))
+
+# Death data.
+# Dados SIM de 2010 a 2019 em todos os Estados.
+dados <- fetch_datasus(year_start = 2010, year_end = 2019,
+                         information_system = "SIM-DO", 
+                         vars = c('CODMUNRES', 'DTOBITO','SEXO','CAUSABAS_O','IDADE'))
+
+# Processar gera dados com Informações espaciais do municipio de residência e idade em anos.
+# CID da Causa básica.
+dados <- process_sim(dados)
+
+# Tidying data 
+# Create variable year
+# Create variable agegroups
+# Group by year, sex, agegroup, UF, CID
+# Create variable with ICD chapter (first letter)
+
+obts <- dados %>%
+  select(DTOBITO, SEXO, IDADEanos, CAUSABAS_O, munResUf) %>%
+  filter(SEXO != "0") %>%
+  replace(is.na(.), 0) %>%
+  mutate(IDADEanos = as.numeric(IDADEanos)) %>%
+  mutate(agegroup = case_when(IDADEanos < 5 ~ "0-4",
+                              IDADEanos >= 5 & IDADEanos < 10 ~ "5-9",
+                              IDADEanos >= 10 & IDADEanos < 15 ~ "10-14",
+                              IDADEanos >= 15 & IDADEanos < 20 ~ "15-19",
+                              IDADEanos >= 20 & IDADEanos < 25 ~ "20-24",
+                              IDADEanos >= 25 & IDADEanos < 30 ~ "25-29",
+                              IDADEanos >= 30 & IDADEanos < 35 ~ "30-34",
+                              IDADEanos >= 35 & IDADEanos < 40 ~ "35-39",
+                              IDADEanos >= 40 & IDADEanos < 45 ~ "40-44",
+                              IDADEanos >= 45 & IDADEanos < 50 ~ "45-49",
+                              IDADEanos >= 50 & IDADEanos < 55 ~ "50-54",
+                              IDADEanos >= 55 & IDADEanos < 60 ~ "55-59",
+                              IDADEanos >= 60 & IDADEanos < 65 ~ "60-64",
+                              IDADEanos >= 65 & IDADEanos < 70 ~ "65-69",
+                              IDADEanos >= 70 & IDADEanos < 75 ~ "70-74",
+                              IDADEanos >= 75 & IDADEanos < 80 ~ "75-79",
+                              IDADEanos >= 80 & IDADEanos < 85 ~ "80-84",
+                              IDADEanos >= 85 ~ "85")) %>%
+  mutate(ano = lubridate::year(DTOBITO)) %>%
+  mutate(ICD1 = substr(CAUSABAS_O,1,1),
+         ICD2 = as.numeric(substr(CAUSABAS_O,2,2))) 
+
+
+
+obts_clean <- obts %>%
+  mutate(ICD_chapter = case_when(
+    ICD1 == "A" | ICD1 == "B" ~ "1.Algumas doenças infecciosas e parasitárias",
+    ICD1 == "C" | (ICD1 == "D" & ICD2 <5) ~ "2.Neoplasias (tumores)",
+    ICD1 == "D" & ICD2 >=5 ~ "3.Doenças do sangue e dos órgãos hematopoiéticos",
+    ICD1 == "E" ~ "4.Doenças endócrinas nutricionais e metabólicas",
+    ICD1 == "F" ~ "5.Transtornos mentais e comportamentais",
+    ICD1 == "G" ~ "6.Doenças do sistema nervoso",
+    ICD1 == "H" & ICD2 <6 ~"7.Doenças do olho e anexos",
+    ICD1 == "H" & ICD2 >=6 ~ "8.Doenças do ouvido e da apófise mastóide",
+    ICD1 == "I" ~ "9.Doenças do aparelho circulatório",
+    ICD1 == "J" ~ "10.Doenças do aparelho respiratório",
+    ICD1 == "K" ~ "11.Doenças do aparelho digestivo",
+    ICD1 == "L" ~ "12.Doenças da pele e do tecido subcutâneo",
+    ICD1 == "M" ~ "13.Doenças sistEma osteomuscular e tecido conjuntivo",
+    ICD1 == "N" ~ "14.Doenças do aparelho geniturinário",
+    ICD1 == "O" ~ "15.Gravidez parto e puerpério",
+    ICD1 == "P" ~ "16.Algumas condições originadas no período perinatal",
+    ICD1 == "Q" ~ "17.Malformações congênitas deformidades e anomalias cromossômicas",
+    ICD1 == "R" ~ "18.Sintomas, sinais e achados clínicos e laboratoriais anormais, não classificados",
+    ICD1 == "S"|ICD1=="T" ~ "19.Lesões envenenamento e outras causas externas",
+    ICD1 == "V"|ICD1=="W"|ICD1 == "X"|ICD1 == "Y" ~ "20.Causas externas de morbidade e mortalidade",
+    ICD1 == "Z" ~ "21.Fatores que influenciam o estado de saúde e o contato com os serviços de saúde",
+    TRUE ~ "sem info")) %>%
+  filter(ICD_chapter != "sem info") %>%
+  rename(Sex = SEXO,
+         UF = munResUf,
+         Year = ano) %>%
+  mutate(Sex = case_when(Sex == "Masculino" ~ "m",
+                         Sex == "Feminino" ~ "f")) %>%
+  mutate(UF = stri_trans_general(str_to_title(UF), id = "Latin-ASCII"))%>%
+  group_by(Year, Sex, agegroup, UF, ICD_chapter) %>%
+  summarise(Deaths = n()) %>%
+  filter(ICD_chapter != "sem info")
+
+
+###### Population data
+library(readxl)
+
+pop_t <- read_excel("data/ProjMunic-2010_2030.xlsx", 
+                    col_types = c("numeric", "text", "text", 
+                                  "skip", "skip", "numeric", "numeric", 
+                                  "numeric", "numeric", "numeric", 
+                                  "numeric", "numeric", "numeric", 
+                                  "numeric", "numeric", "numeric", 
+                                  "numeric", "numeric", "numeric", 
+                                  "numeric", "numeric", "numeric", 
+                                  "numeric", "numeric", "skip"),
+                    col_names = c('Year', 'UF', 'Sex',
+                                  "0-4", "5-9", "10-14", "15-19",
+                                  "20-24", "25-29",  "30-34", "35-39",
+                                  "40-44", "45-49", "50-54", "55-59",
+                                  "60-64", "65-69", "70-74",
+                                  "75-79", "80-84", "85-89","90+"))
+
+pop_t_clean <- pop_t %>%
+  filter(!is.na(Year)) %>%
+  gather("agegroup", "pop", 4:22) %>%
+  mutate(agegroup = str_replace_all(agegroup, "a", "-"),
+         agegroup = str_replace_all(agegroup, " ", "")) %>%
+  mutate(agegroup = case_when(agegroup == "85-89" ~ "85",
+                              agegroup == "90+" ~ "85",
+                              TRUE ~ agegroup)) %>%
+  group_by(Year,Sex,agegroup, UF) %>%
+  summarise(pop = sum(pop)) %>%
+  mutate(UF = stri_trans_general(str_to_title(UF), id = "Latin-ASCII"))
+
+# UF shapefile
+UF_shp <- read_sf("data/BR_UF_2020.shp") %>%
+  mutate(NM_UF = stri_trans_general(str_to_title(NM_UF), id = "Latin-ASCII")) %>%
+  sf::st_transform('+proj=longlat +datum=WGS84')
+
+# Manter apenas as bases limpas e salvar em RData.
+rm(list=setdiff(ls(), c("obts_clean", "pop_t_clean", "standard_pop_clean", "UF_shp")))
+
+save(obts_clean, pop_t_clean, standard_pop_clean,UF_shp, file = "data.RData")
+
---
+++ app.R
@@ -0,0 +1,225 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+
+library(shiny)
+library(shinydashboardPlus)
+library(shinyWidgets)
+library(leaflet)
+library(tidyverse)
+library(plotly)
+library(ggrepel)
+library(shiny.i18n)
+load("C:/projetos/BrazilMortalityRate/data.RData")
+
+# i18n <- Translator$new(translation_json_path='translations/translation.json')
+# i18n$set_translation_language('en')
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # shiny.i18n::usei18n(i18n),
+  # tags$div(
+  #   style='float: right;',
+  #   selectInput(
+  #     inputId='selected_language',
+  #     label=i18n$t('Change language'),
+  #     choices = i18n$get_languages(),
+  #     selected = i18n$get_key_translation()
+  #   )),
+  ## Stlysh CSS
+  
+  h1(id="tag1", "Dados abertos <> Conhecimento livre <> Saúde Pública."),
+  
+  h1(id="tag2", "Taxa de mortalidade padronizada por sexo e faixa etária nos Estados do Brasil (2010-2019)."),
+  
+  tags$head(
+    tags$style(HTML(
+      '#tag1 {color: #c1946a; 
+                  background-color:#405d27;
+                  border:2px solid #c1946a;
+                  border-radius: 15px 50px 30px;
+                  font-size: 20px;
+                  font-style: bold;
+                  text-align:right;
+                  padding-top:10px;
+                  padding-bottom:10px;
+                  font-family:"Monaco",Georgia,Serif;
+                  }
+    #tag2 {color: #405d27; 
+                  background-color: white;
+                  border-radius: 15px 50px 30px;
+                  border:2px solid #c1946a;
+                  font-size: 25px;
+                  font-style: bold;
+                  text-align:center;
+                  padding-top:10px;
+                  padding-bottom:10px;
+                  font-family:"Monaco",Georgia,Serif;
+    }
+    .tabbable > .nav > li > a[data-value="Mapa"] 
+      {background-color: #405d27; 
+      border:2px solid #c1946a;
+    color:#c1946a;
+    width: 8vw;
+    text-align:center;
+    font-style:bold;
+    font-size:20px;
+    font-family:"Monaco",Georgia,Serif;
+    }
+    .tabbable > .nav > li > a[data-value="Dados"] 
+      {background-color: #405d27;
+      border:2px solid #c1946a;
+    color:#c1946a;
+    width: 8vw;
+    text-align:center;
+    font-size:20px;
+    font-family:"Monaco",Georgia,Serif;
+      }
+      .tabbable > .nav > li > a[data-value="README"] 
+      {background-color: white; 
+      border:2px solid #c1946a;
+    color:#c1946a;
+    width: 8vw;
+    text-align:center;
+    font-style:bold;
+    font-size:20px;
+    font-family:"Monaco",Georgia,Serif;
+    }
+  '))),
+  
+  # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+      
+        sidebarPanel(
+          h4("O que é e como utilizar?"),
+          style = '
+          border-radius: 15px 50px 30px;
+          border:2px solid #405d27;
+          font-family:"Monaco",Georgia,Serif;
+          padding-top:30px;
+          padding-bottom:10px;
+          font-size: 15px;',
+
+          helpText("O dashboard oferece uma vizualização interativa da distribuição espacial nas Unidades Federativas do Brasil das taxas de mortalidade padronizadas por sexo e feixa etária
+                   de acordo com as causas básicas agrupadas nos capítulos do CID-10. 
+                   Para saber mais a respeito do processo de construção, clique na aba 'READme' do painel.
+                  
+                   Para criar um mapa específico:
+                   Selecione um ano, o tipo de taxa (padronizada ou bruta) e o capítulo do CID-10 de interesse."),
+          selectInput("year",
+                      "Ano:",
+                      choices = c('2010','2011','2012','2013','2014','2015','2016','2017','2018','2019')),
+          selectInput("taxa",
+                      "Taxa:",
+                      choices = c('Padronizada', 'Bruta')),
+          prettyRadioButtons("cid",
+                             'Causa básica por Capítulo (CID-10):',
+                            choices = unique(obts_clean$ICD_chapter),
+                             shape = "square",
+                             status = 'warning',
+                             bigger = T,
+                             animation = "smooth")),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+          
+          tabsetPanel(
+            tabPanel("Mapa",
+                     plotOutput("map1", width = "100%", height = "900px")),
+            tabPanel("Dados", tableOutput("table1")),
+            tabPanel("README", 
+                     includeMarkdown("Hello World"))
+          )
+        )
+    ))
+  
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # observeEvent(input$selected_language, {
+  #   update_lang(session, input$selected_language)
+  # })
+  
+  obts1 <- reactive({
+    obts_clean %>%
+      filter(Year == input$year & ICD_chapter == input$cid)
+  })
+  
+  pop_t1 <- reactive(
+    pop_t_clean %>%
+      filter(Year == input$year)
+  )
+  
+  df1 <- reactive(
+    right_join(obts1(), pop_t1(), by = c('Sex', 'agegroup', 'UF')) 
+  )
+  
+  df2 <- reactive(
+    left_join(df1(), standard_pop_clean, by = c('Sex', 'agegroup'))
+  )
+  
+  mortality_ds_rate_phe <- 
+    reactive(
+      df2() %>%
+        group_by(UF) %>%
+        PHEindicatormethods::phe_dsr(
+          x = Deaths,                 # column with observed number of events
+          n = pop.x,             # column with non-standard pops for each stratum
+          stdpop = pop.y,               # standard populations for each stratum
+          stdpoptype = "field") %>%
+        mutate(Bruta = total_count/total_pop * 100000)%>%
+        rename(Padronizada = value)
+    )
+  
+  output$table1 <- renderTable({
+    mortality_ds_rate_phe()
+  })
+  
+  map <- reactive(
+    left_join(UF_shp, mortality_ds_rate_phe(), by = c('NM_UF'= 'UF')) %>%
+      mutate(inc_cat = cut(get(input$taxa), include.lowest = T,
+                           breaks = c(quantile(get(input$taxa), by = 0.2, na.rm = T))))
+    )
+  
+ 
+ 
+  ## Mapa
+  
+  # Paleta de cores da gradação de taxas de mortaldiade
+  
+  pal <- hcl.colors(5, "Heat", rev = TRUE, alpha = 0.7)
+  
+  output$map1 <- renderPlot({
+    
+    ggplot(map()) +
+      geom_sf(aes(fill = inc_cat),
+              color = "black",
+              lwd = 0.35) + 
+      ggtitle(paste0("Taxa de mortalidade ",input$taxa ," por ", input$cid, " em ", input$year))+
+      labs(fill = "", x="", y ="")+
+      scale_fill_manual(values = pal,
+                        drop = FALSE,
+                        na.value = "#c1946a")+
+      geom_sf_label(aes(label = round(get(input$taxa),1)), colour = "black", na.rm = T, size = 4)+
+      coord_sf()+
+      theme_bw()+
+      theme(plot.title = element_text(size = 20, hjust = 0.5),
+            legend.position = "left",
+            legend.direction = "vertical",
+            legend.text = element_text(size = 15),
+            panel.border = element_rect(colour = "#405d27", fill=NA, size=2),
+            axis.text.x = element_text(size = 10),
+            axis.text.y = element_text(size = 10))
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
